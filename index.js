b46f0f4deebfeb62635ba30b97746a89a87f649d
Charles Morris <percworld@gmail.com>
2021-01-04 11:48:00 -0700

    add functions display data and change classlists

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+var ball = document.querySelector('.ball');
 var questionInput = document.querySelector('.question');
 var roll = document.querySelector('.roll');
 var clear = document.querySelector('.clear');
@@ -13,10 +14,30 @@ function getRandomIndex(array) {
 };
 
 function getData() {
+  question = questionInput;
   answer = answersArray[getRandomIndex(answersArray)];
+  userQuestion.innerText = question;
+  userAnswer.innerText = answer;
+  displayData();
   console.log(answer);
   console.log(answersArray);
 };
+
+displayData() {
+  hide(questionInput);
+  hide(roll);
+  hide(ball);
+  show(userQuestion);
+  show(userAnswer);
+}
+
+clearField() {
+  show(questionInput);
+  show(roll);
+  show(ball);
+  hide(userQuestion);
+  hide(userAnswer);
+}
 
 function hide(e) {
   e.classList.add('hidden');
@@ -25,3 +46,26 @@ function hide(e) {
 function show(e) {
   e.classList.remove('hidden');
 };
+
+var answersArray = [
+  'It is certain.',
+  'It is decidedly so.',
+  'Without a doubt.',
+  'Yes - definitely.',
+  'You may rely on it.',
+  'As I see it, yes.',
+  'Most likely.',
+  'Outlook good.',
+  'Yes.',
+  'Signs point to yes.',
+  'Reply hazy, try again.',
+  'Ask again later.',
+  'Better not tell you now.',
+  'Cannot predict now.',
+  'Concentrate and ask again.',
+  'Don\'t count on it.',
+  'My reply is no.',
+  'My sources say no.',
+  'Outlook not so good.',
+  'Very doubtful.'
+];
